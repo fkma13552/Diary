256c1db0f9e2d0a22d49f6908f9660f8c1878771
fkma13552 <ozonplay123@gmail.com>
2021-03-03 00:02:26 +0200

    Updated UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,25 +8,28 @@ MainWindow::MainWindow(INotesController& notesController, QWidget *parent)
 : QMainWindow(parent)
 {
 
-setWindowTitle("App 1");
-setGeometry(100, 100, 500, 500);
+setWindowTitle("My diary");
+setGeometry(100, 100, 400, 500);
 setMinimumSize(200, 200);
 
 this->notesController = &notesController;
 notes = new QListWidget(this);
-notes->addItem(QString("Text"));
-//notes->resize( 300, 300 );
+notes->addItem(QString("Here will be shown list of writings"));
+notes->resize( 400, 100 );
 
 
-//outputField = new QTextEdit("Some text here.\nIt is readonly.", this);
-//outputField->setGeometry(10, 10, 180, 150);
-//outputField->setReadOnly(true);
+outputField = new QTextEdit("Some text here (detailed writings).\nIt is readonly.", this);
+outputField->setGeometry(10, 110, 190, 100);
+outputField->setReadOnly(true);
 
-//inputField = new QLineEdit(this);
-//inputField->setGeometry(10, 170, 140, 20);
+inputField = new QLineEdit(this);
+inputField->setGeometry(10, 220, 50, 20);
 
-QPushButton *okButton = new QPushButton("OK", this);
-okButton->setGeometry(155, 170, 35, 20);
+inputField = new QLineEdit(this);
+inputField->setGeometry(10, 250, 240, 20);
+
+QPushButton *okButton = new QPushButton("Add", this);
+okButton->setGeometry(260, 250, 35, 20);
 connect(okButton, SIGNAL(released()), this, SLOT(handleButton()));
 
 calendar = new QCalendarWidget(this);
